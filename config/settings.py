d8c13ff5f240775e4e51df5cb4089fb6a2cbaf55
Novey Am <noveycea@gmail.com>
2026-03-17 18:19:19 -0700

    removed old database from sqlite to rds

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,7 @@ SECRET_KEY = (
     or "unsafe-dev-key"
 )
 
+# DATABASE
 DB_NAME = os.getenv("DB_NAME") or read_secret_file("/mnt/secrets/DB_NAME")
 DB_USER = os.getenv("DB_USER") or read_secret_file("/mnt/secrets/DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD") or read_secret_file("/mnt/secrets/DB_PASSWORD")
@@ -121,18 +122,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'config.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
