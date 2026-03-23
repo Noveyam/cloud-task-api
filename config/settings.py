1847171c9235c7138e97b30f3e643eb3f3abe941
Novey Am <noveycea@gmail.com>
2026-03-23 15:52:15 -0700

    allow sqlite fallback for Django check

--- config/settings.py
+++ config/settings.py
@@ -44,7 +44,7 @@ DB_PASSWORD = os.getenv("DB_PASSWORD") or read_secret_file("/mnt/secrets/DB_PASS
 DB_HOST = os.getenv("DB_HOST") or read_secret_file("/mnt/secrets/DB_HOST")
 DB_PORT = os.getenv("DB_PORT") or read_secret_file("/mnt/secrets/DB_PORT") or "5432"
 
-RUNNING_TESTS = "test" in sys.argv
+RUNNING_CI_SAFE_COMMAND = any(cmd in sys.argv for cmd in ["test", "check"])
 
 if DB_NAME and DB_USER and DB_PASSWORD and DB_HOST:
     DATABASES = {
@@ -57,7 +57,7 @@ if DB_NAME and DB_USER and DB_PASSWORD and DB_HOST:
             "PORT": DB_PORT,
         }
     }
-elif RUNNING_TESTS:
+elif RUNNING_CI_SAFE_COMMAND:
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.sqlite3",
